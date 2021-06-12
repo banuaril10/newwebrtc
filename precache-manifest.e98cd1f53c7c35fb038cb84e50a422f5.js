5081230fa091bbbcb4ef51f97668836058d2db58
banuaril10 <banuaril100@gmail.com>
2021-06-12 21:36:01 +0700

    Updates

---
+++ precache-manifest.e98cd1f53c7c35fb038cb84e50a422f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d5ac7a3d491e5438bdb03db06147b1",
+    "url": "/newwebrtc/index.html"
+  },
+  {
+    "revision": "efb43784b874e96c7b63",
+    "url": "/newwebrtc/static/css/main.0fa05b8b.chunk.css"
+  },
+  {
+    "revision": "505d8093ca56ce09f4cb",
+    "url": "/newwebrtc/static/js/2.ab1b521a.chunk.js"
+  },
+  {
+    "revision": "efb43784b874e96c7b63",
+    "url": "/newwebrtc/static/js/main.e171adea.chunk.js"
+  },
+  {
+    "revision": "d6c748fc8682eadf66d0",
+    "url": "/newwebrtc/static/js/runtime-main.c97fa7ea.js"
+  }
+]);
